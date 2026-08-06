35a565fb5e327115888434bc6c080364483b145e
Chaithu Sridhar <ygf056@usask.ca>
2026-08-06 10:45:46 -0600

    Add PostgreSQL database connection

--- auth-service/go.mod
+++ auth-service/go.mod
@@ -1,3 +1,8 @@
 module github.com/chaithuSridhar7/identity-management-system/auth-service
 
 go 1.26.5
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
---
+++ auth-service/internal/database/database.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func Connect() (*sql.DB, error) {
+
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("error loading .env file: %v", err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host,
+		port,
+		user,
+		password,
+		dbname,
+	)
+
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
